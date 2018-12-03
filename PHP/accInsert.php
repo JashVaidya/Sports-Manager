d121a2cb6f23e077e8bc13002f422ee5dc738f7a
William Mabry <wmabry2@radford.edu>
2018-12-03 15:57:38 -0500

    Update
    Fixing errors

--- PHP/accInsert.php
+++ PHP/accInsert.php
@@ -4,8 +4,14 @@
 
         $conn = mysqli_connect("localhost","wmabry2","Williamowns1","wmabry2");
 
-        $username = mysqli_real_escape_string($conn,$_POST['user']);
-        $password = sha1(mysqli_real_escape_string($conn,$_POST['pass']));
+	
+
+        $username = $_POST['username'];
+        $password = sha1($_POST['password']);
+
+	echo $username;
+	echo $password;
+
       
 
         /**
@@ -20,7 +26,8 @@
 
         $result = mysqli_query($conn,"INSERT into wmabry2.users (username, password) VALUES 
         							('$username', '$password')");
-        /**
+
+	 /**
          * Checks to see if the query failed to insert the new user into the database and 
          * if it does fail echo that the username already exists and to choose a different one
          * Possibly exchange this echo statement for a javascript alert() statement then
@@ -29,6 +36,9 @@
         if (!$result) {
                 echo "Account already exists! Please choose a different username.";  
         }
+	else {
+		echo "Account inserted!";
+	}
 
         mysqli_close($conn);
 ?>
