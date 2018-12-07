e5441c8f8e3e991f1fce650e8656fccdaf0c84df
William Mabry <wmabry2@radford.edu>
2018-12-07 16:45:34 -0500

    Fixed login functionality for account.php
    Checks to see if user and password are valid, if so, loads account.php with working username and such in the nav bar. If not, redirects them back to login.php for them to try again(would like some kind of error message here to notify them their login failed)

--- PHP/account.php
+++ PHP/account.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,18 +11,43 @@ session_start();
 </head>
 
 <body>
-<?php
-
-
-
-
-?>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="index.php">SM</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
+    <?php
+session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $conn = mysqli_connect("localhost","wmabry2","Williamowns1","wmabry2");
+
+        //get input
+        $loginUser = mysqli_real_escape_string($conn, $_POST['loginUser']);
+        $loginPass = sha1(mysqli_real_escape_string($conn, $_POST['loginPass']));
+
+        //query input
+        $sql = "SELECT username FROM users WHERE username = '$loginUser' AND password = '$loginPass'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
+
+        //count row result which should be = 1 if sucsessful login
+        $count = mysqli_num_rows($result);
+        if ($count == 1) {
+            session_register("loginUser");
+            //set the users login as the session logged in user for future pages
+            $_SESSION['login_user'] = $loginUser;
+        } else {
+            //invalid
+            echo '<script language="javascript">';
+            echo 'alert("Invalid Username or Password!")';
+            echo '</script>';
+            header('location: login.php');
+        }
+    }
+?>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
@@ -37,7 +59,7 @@ session_start();
                 <a class="nav-link" href="teamSearch.php">Search</a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="account.php">User</a>
+                <a class="nav-link" href="account.php"><?php echo $_SESSION['login_user']; ?></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php">Sign Out</a>
@@ -52,7 +74,7 @@ session_start();
             <div class="col .text-center">
                 </br></br>
                 <img src="../Assets/jash.jpg" alt="Picture of Jash" class="img-thumbnail rounded">
-                <h5>jvaidya</h5>
+                <h5><?php echo $_SESSION['login_user']; ?></h5>
                 <h5>Favorite team: Cowboys</h5>
                 <a href="accountSettings.php"><button class="btn btn-sm btn-dark">Edit Account</button></a>
                 <br><br>
