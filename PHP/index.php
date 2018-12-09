57a588040a04f8076370ceeff85be5f95e05d3d7
Nick Shillingford <nshillingford21@gmail.com>
2018-12-09 18:58:05 -0500

    change login.php to index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php session_start();?>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -6,11 +7,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/custom.css">
-    <title>Welcome</title>
+    <title>Login</title>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="index.php">SM</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
 
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item active">
                 <a class="nav-link" href="login.php">Login</a>
             </li>
             <li class="nav-item">
@@ -33,45 +33,46 @@
     </div>
 </nav>
 
-<div id="homebg" class="jumbotron">
-
-	<div class="container-forms">
-		<div class="glass-search team">
-		  <form class="form-signin">
-			<input id="team-input" type="text" class="form-ui" style="left:6%;text-indent:100px;" 
-			placeholder="Team" required="">
-			
-			<button class="btn btn-md btn-dark btn-block form-btn" 
-			style="margin-top:33%;left:5.5%;" type="submit">Search</button>
-		  </form>
-		</div>
-		
-		<div class="glass-search player">
-		  <form class="form-signin">
-			<input id="player-input" type="text" class="form-ui" style="left:6%;text-indent:100px;" 
-			placeholder="Player" required="">
-			
-			<button class="btn btn-md btn-dark btn-block form-btn" 
-			style="margin-top:33%;left:5.5%;" type="submit">Search</button>
-		  </form>
-		</div>
-	</div>
+<div id="loginbg" class="jumbotron">
+  <div class="glass-signin-join">
+    <form onsubmit="return val()" action="account.php" class="form-signin" method="post">
+      <input id="un" style="margin-top:12%;left:10%;" name="loginUser" type="text" class="form-ui" placeholder="Username">
+      <input id="pw" style="margin-top:36%;left:10%;" name="loginPass" type="password" class="form-ui" placeholder="Password">
+	  <div id="error" style="left:18%;">
+		<p id="message"></p>
+	  </div>
+      <button id="btn" style="margin-top:68%;left:10.5%;" class="btn btn-md btn-dark btn-block form-btn" type="submit">
+		Sign in
+	  </button>
+    </form>
+  </div>
 </div>
-
 <footer class="footer">
     <div class="container">
         <span class="text-muted">Team Contact Info Here</span>
     </div>
 </footer>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
 </body>
+
+<script>
+	function val() {
+		let error = document.querySelector('#error');
+		let message = document.querySelector('#message');
+		let un = document.querySelector('#un');
+		let pw = document.querySelector('#pw');
+		
+		if ((un.value === '' || pw.value === '')) {
+			message.innerHTML = 'Fields must contain characters.';
+			error.style.visibility = 'visible';
+			return false;
+		}
+		else if (un.value.length > 20 || pw.value.length > 20) {
+			message.innerHTML = 'Fields must be less than 20 characters.';
+			error.style.visibility = 'visible';
+			return false;
+		}
+		return true;
+	}
+</script>
+
 </html>
